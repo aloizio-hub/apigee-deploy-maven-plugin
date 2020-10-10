2e580f3f344fc422eba295c231d7b9844a8d1cce
root <root@ansible3.us-east1-b.c.estudo-kubernetes-277718.internal>
2020-10-10 15:54:21 +0000

    fisrt commit

---
+++ pipeline-santander/src/main/java/br/com/santander/maven/classe2.java
@@ -0,0 +1,16 @@
+package br.com.santander.maven;
+
+/**
+ * Hello world teste 2!
+ *
+ */
+public class classe2
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World teste2!" );
+    }
+}
+
+
+
